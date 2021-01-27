78e6db91386240f8cc2d352c3c1bf45ff2e0acc3
dima_beel <sedrpdasavushkinby@gmail.com>
2021-01-27 15:42:36 +0300

    Metadata from file for Inventory

--- CarLotMVC/AutoLotDAL.Models/InventoryPartial.cs
+++ CarLotMVC/AutoLotDAL.Models/InventoryPartial.cs
@@ -1,15 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models
 {
+	[MetadataType(typeof(InventoryMetaData))]
 	public partial class Inventory
 	{
 		public override string ToString()
 		{
 			// Since the PetName column could be empty, supply
 			// the default name of **No Name**.
-			return $"{this.PetName ?? "**No Name**"} is a {this.Color} {this.Make} with ID {this.Id}.";
+			return $"{PetName ?? "**No Name**"} is a {Color} {Make} with ID {Id}.";
 		}
+
 	    [NotMapped]
 	    public string MakeColor => $"{Make} + ({Color})";
 
