c77432dd309d026833ebfd521532177af6faf06a
Dzmitri Umetsky <dima_beel@mail.ru>
2021-01-31 15:02:36 +0300

    Web api end

--- CarLotMVC/CarLotMVC/Controllers/InventoryController.cs
+++ CarLotMVC/CarLotMVC/Controllers/InventoryController.cs
@@ -4,51 +4,67 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using AutoLotDAL.EF;
 using AutoLotDAL.Models;
-using AutoLotDAL.Repos;
+using Newtonsoft.Json;
 
 namespace CarLotMVC.Controllers
 {
     public class InventoryController : Controller
     {
-        private InventoryRepo repo = new InventoryRepo();
+        private string apiUrl = "http://localhost:55785/api/Inventory";
 
         // GET: Inventory
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View(repo.GetAll());
+            var client = new HttpClient();
+            var response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var items = JsonConvert.DeserializeObject<List<Inventory>>(
+                    await response.Content.ReadAsStringAsync());
+                return View(items);
+            }
+
+            return HttpNotFound();
         }
 
         // GET: Inventory/Details/5
-        public ActionResult Details(int? id)
+        public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = repo.GetOne(id);
-            if (inventory == null)
+
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{apiUrl}/{id.Value}");
+
+            if (response.IsSuccessStatusCode)
             {
-                return HttpNotFound();
+                var itemDetails = JsonConvert.DeserializeObject<Inventory>(
+                    await response.Content.ReadAsStringAsync());
+                return View(itemDetails);
             }
-            return View(inventory);
+
+            return HttpNotFound();
         }
 
-        // GET: Inventory/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Inventory/Create
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        // GET: Inventory/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Make, Color, PetName")] Inventory inventory)
+        public async Task<ActionResult> Create(
+            [Bind(Include = "Make, Color, PetName")] Inventory inventory)
         {
             if (!ModelState.IsValid)
             {
@@ -57,31 +73,44 @@ namespace CarLotMVC.Controllers
 
             try
             {
-                repo.Add(inventory);
+                var client = new HttpClient();
+                string json = JsonConvert.SerializeObject(inventory);
+                var stringContent = new StringContent(json, Encoding.UTF8,
+                    "application/json");
+                var response = await client.PostAsync(apiUrl, stringContent);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
             }
             catch (Exception ex)
             {
-                ModelState.AddModelError(string.Empty,
-                    $"Unable to create record: {ex.Message}");
-                return View(inventory);
+                ModelState.AddModelError(string.Empty, $"Unable to create" +
+                    $"record: {ex.Message}");
             }
 
-            return RedirectToAction("Index");
+            return View(inventory);
         }
 
         // GET: Inventory/Edit/5
-        public ActionResult Edit(int? id)
+        public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = repo.GetOne(id);
-            if (inventory == null)
+
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{apiUrl}/{id.Value}");
+
+            if (response.IsSuccessStatusCode)
             {
-                return HttpNotFound();
+                var itemDetails = JsonConvert.DeserializeObject<Inventory>(
+                    await response.Content.ReadAsStringAsync());
+                return View(itemDetails);
             }
-            return View(inventory);
+
+            return HttpNotFound();
         }
 
         // POST: Inventory/Edit/5
@@ -89,62 +118,67 @@ namespace CarLotMVC.Controllers
         // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id, Make, Color, PetName, Timestamp")] Inventory inventory)
+        public async Task<ActionResult> Edit([Bind(Include = "Id, Make, Color, PetName, Timestamp")] Inventory inventory)
         {
             if (!ModelState.IsValid)
             {
                 return View(inventory);
             }
 
-            try
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(inventory);
+            var stringContent = new StringContent(json, Encoding.UTF8,
+                "application/json");
+            var response = await client.PutAsync($"{apiUrl}/{inventory.Id}",
+                stringContent);
+            if (response.IsSuccessStatusCode)
             {
-                repo.Save(inventory);
-            }
-            catch (DBConcurrencyException ex)
-            {
-                ModelState.AddModelError(string.Empty,
-                    $"Unable to save the record. Another user has updated" +
-                    $" it. {ex.Message}");
-                return View(inventory);
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(string.Empty,
-                    $"Unable to save the record. {ex.Message}");
-                return View(inventory);
+                return RedirectToAction("Index");
             }
 
-            return RedirectToAction("Index");
+            return View(inventory);
         }
 
         // GET: Inventory/Delete/5
-        public ActionResult Delete(int? id)
+        public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = repo.GetOne(id);
-            if (inventory == null)
+
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{apiUrl}/{id.Value}");
+
+            if (response.IsSuccessStatusCode)
             {
-                return HttpNotFound();
+                var inventory = JsonConvert.DeserializeObject<Inventory>(
+                    await response.Content.ReadAsStringAsync());
+                return View(inventory);
             }
-            return View(inventory);
+
+            return HttpNotFound();
         }
 
         // POST: Inventory/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed([Bind(Include = "Id, Timestamp")] Inventory inventory)
+        public async Task<ActionResult> Delete(
+            [Bind(Include = "Id, Timestamp")] Inventory inventory)
         {
             try
             {
-                repo.Delete(inventory);
-            }
-            catch (DBConcurrencyException ex)
-            {
-                ModelState.AddModelError(string.Empty, $"Unable to delete" +
-                    $"record. Another user updated the record. {ex.Message}");
+                var client = new HttpClient();
+                var requestMessage = new HttpRequestMessage(
+                    HttpMethod.Delete, $"{apiUrl}/{inventory.Id}")
+                {
+                    Content = new StringContent(
+                        JsonConvert.SerializeObject(inventory),
+                        Encoding.UTF8, "application/json")
+                };
+
+                var response = await client.SendAsync(requestMessage);
+                return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
@@ -152,16 +186,12 @@ namespace CarLotMVC.Controllers
                     $"record: {ex.Message}");
             }
 
-            return RedirectToAction("Index");
+            return View(inventory);
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                repo.Dispose();
-            }
-            base.Dispose(disposing);
+             base.Dispose(disposing);
         }
     }
 }
