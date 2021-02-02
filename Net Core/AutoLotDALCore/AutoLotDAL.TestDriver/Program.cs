de1fd2c521fbc20371919a066f203e86246c33af
Dzmitri Umetsky <dima_beel@mail.ru>
2021-02-02 21:36:19 +0300

    Basic set up

---
+++ Net Core/AutoLotDALCore/AutoLotDAL.TestDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoLotDAL.TestDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Net Core/AutoLotDALCore/AutoLotDALCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AutoLotDALCore
-{
-    public class Class1
-    {
-    }
-}
