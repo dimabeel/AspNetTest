701b1fa6378abf96c368b2f78c41e684e6856c43
Dzmitri Umetsky <dima_beel@mail.ru>
2021-02-07 20:35:45 +0300

    Implement EmailTagHelper.cs

---
+++ Net Core/AutoLotMVCCore/AutoLotMVCCore/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoLotMVCCore.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailName { get; set; }
+
+        public string EmailDomain { get; set; }
+
+        public override void Process(TagHelperContext context,
+            TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var adress = $"{EmailName}@{EmailDomain}";
+            output.Attributes.SetAttribute("href", $"mailto:{adress}");
+            output.Content.SetContent(adress);
+        }
+    }
+}
