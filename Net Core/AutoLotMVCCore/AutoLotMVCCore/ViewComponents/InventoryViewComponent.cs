4b6d24a5d92257248120f374e38972c43d26990f
dima_beel <sedrpdasavushkinby@gmail.com>
2021-02-10 16:43:33 +0300

    Work on view components

---
+++ Net Core/AutoLotMVCCore/AutoLotMVCCore/ViewComponents/InventoryViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AutoLotDALCore.Repos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace AutoLotMVCCore.ViewComponents
+{
+    public class InventoryViewComponent : ViewComponent
+    {
+        private readonly IInventoryRepo repo;
+        
+        public InventoryViewComponent(IInventoryRepo repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cars = repo.GetAll(x => x.Make, true);
+            if (cars != null)
+            {
+                return View("InventoryPartialView", cars);
+            }
+
+            return new ContentViewComponentResult("Unable to locate records.");
+        }
+    }
+}
